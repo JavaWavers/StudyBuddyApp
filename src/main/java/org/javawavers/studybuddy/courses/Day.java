3f1e4f0d5d6d679b0cc9c56953e1f7a5c3f57b25
IoannaGiag <t8230022@aueb.gr>
2025-01-03 23:19:50 +0200

    Update Day.java

--- src/main/java/org/javawavers/studybuddy/courses/Day.java
+++ src/main/java/org/javawavers/studybuddy/courses/Day.java
@@ -7,10 +7,13 @@ import java.time.LocalDate;
 import java.time.DayOfWeek;
 
 public class Day {
+    LocalDate date;
     int availability;
     List<Task> dayTasks;
 
-    public Day (int availability, List<Task> dayTasks) {
+
+    public Day (LocalDate date, int availability, List<Task> dayTasks) {
+        this.date = date;
         this.availability = availability;
         this.dayTasks = dayTasks;
     }
