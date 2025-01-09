e749626fbd2f7c166e80c2aca80c9db5f065e34c
Domprou AnnaEleni <t8230035@aueb.gr>
2025-01-09 22:55:06 +0200

    correct import statement

--- src/main/java/org/javawavers/studybuddy/StudyBuddyApp.java
+++ src/main/java/org/javawavers/studybuddy/StudyBuddyApp.java
@@ -107,9 +107,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import org.javawavers.studybuddy.ui_ux.HomePage;
-import org.javawavers.studybuddy.ui_ux.Popupdia;
-import org.javawavers.studybuddy.ui_ux.SceneManager;
+import main.java.org.javawavers.studybuddy.ui_ux.HomePage;
+import main.java.org.javawavers.studybuddy.ui_ux.SceneManager;
 
 /*
  * TODO:
