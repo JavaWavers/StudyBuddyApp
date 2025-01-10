875480611d3c197728522019c1a0e9d1a4cdc64c
Domprou AnnaEleni <t8230035@aueb.gr>
2025-01-10 12:16:06 +0200

    create Week class

--- src/main/java/org/javawavers/studybuddy/calculations/Week.java
+++ src/main/java/org/javawavers/studybuddy/calculations/Week.java
@@ -1,4 +1,22 @@
 package org.javawavers.studybuddy.calculations;
 
+import org.javawavers.studybuddy.courses.ScheduledTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Week {
+    public List <Day> daysOfWeek;
+    //constructor
+    public Week (){
+        daysOfWeek = new ArrayList<>();
+    }
+    //returns the whole list
+    private List<Day> getDaysOfWeek(){
+        return daysOfWeek;
+    }
+    //returns the specified task from the list
+    private Day getTheDay(int index){
+        return daysOfWeek.get(index);
+    }
 }
