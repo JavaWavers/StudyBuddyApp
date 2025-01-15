c6503433d6ae9849aa6f58df1afca8d749a5d5d6
DanPeng <t8230218@aueb.gr>
2025-01-15 08:13:44 +0200

    update

--- src/main/java/org/javawavers/studybuddy/calculations/CreateWeekDay.java
+++ src/main/java/org/javawavers/studybuddy/calculations/CreateWeekDay.java
@@ -95,7 +95,7 @@ public class CreateWeekDay {
                   subject.getCourseName(),
                   taskType,
                   StaticUser.staticUser.getUserID(),
-                  numOfDay
+                  1
                   );
         }
       }
